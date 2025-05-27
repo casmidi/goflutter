de458ce1b43270a1a9efa48d8a8c35ff79787df9
casmidi <casmidiasli@yahoo.co.id>
2025-05-27 11:34:25 +0700

    Menambahkan file13.dart

---
+++ dart_basic/lib/latih14.dart
@@ -0,0 +1,31 @@
+void main() {
+  int noOfMonth = 13;
+  // Check the no of month
+  if (noOfMonth == 1) {
+    print("The month is jan");
+  } else if (noOfMonth == 2) {
+    print("The month is feb");
+  } else if (noOfMonth == 3) {
+    print("The month is March");
+  } else if (noOfMonth == 4) {
+    print("The month is April");
+  } else if (noOfMonth == 5) {
+    print("The month is Mei");
+  } else if (noOfMonth == 6) {
+    print("The month is June");
+  } else if (noOfMonth == 7) {
+    print("The month is July");
+  } else if (noOfMonth == 8) {
+    print("The month is Aug");
+  } else if (noOfMonth == 9) {
+    print("The month is Sept");
+  } else if (noOfMonth == 10) {
+    print("The month is Oct");
+  } else if (noOfMonth == 11) {
+    print("The month is Nov");
+  } else if (noOfMonth == 12) {
+    print("The month is Dec");
+  } else {
+    print("Invalid option given.");
+  }
+}
