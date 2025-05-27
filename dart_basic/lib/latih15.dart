6b450b58e6169757825baaeac691d0d932dcb87f
casmidi <casmidiasli@yahoo.co.id>
2025-05-27 16:16:52 +0700

    Menambahkan file latih15.dart

---
+++ dart_basic/lib/latih15.dart
@@ -0,0 +1,15 @@
+void main() {
+  int num1 = 1200;
+  int num2 = 1000;
+  int num3 = 150;
+
+  if (num1 > num2 && num1 > num3) {
+    print("Num 1 is greater : i.e $num1");
+  }
+  if (num2 > num1 && num2 > num3) {
+    print("Num 2 is greather : iee $num2");
+  }
+  if (num3 > num1 && num3 > num2) {
+    print("Num 3 is greather : i.e $num3");
+  }
+}
