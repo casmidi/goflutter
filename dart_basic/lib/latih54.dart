65941df91c4f124181ce120610f737a821610e32
casmidi <casmidiasli@yahoo.co.id>
2025-06-02 15:02:17 +0700

    Penambahan file latih54.dart

---
+++ dart_basic/lib/latih54.dart
@@ -0,0 +1,44 @@
+void main() {
+  var noOfMoneth = 5;
+  switch (noOfMoneth) {
+    case 1:
+      print("Selected month is Januari.");
+      break;
+    case 2:
+      print("Selected month is Februari");
+      break;
+    case 3:
+      print("Selected month is March");
+      break;
+    case 4:
+      print("Selected month is April");
+      break;
+    case 5:
+      print("Selected month is May.");
+      break;
+    case 6:
+      print("selected month is June");
+      break;
+    case 7:
+      print("Selected month is July.");
+      break;
+    case 8:
+      print("Selected month is August.");
+      break;
+    case 9:
+      print("Selected month is september");
+      break;
+    case 10:
+      print("Selected month is October.");
+      break;
+    case 11:
+      print("selected month is November.");
+      break;
+    case 12:
+      print("Selected month is Desember.");
+      break;
+    default:
+      print("Invalid month");
+      break;
+  }
+}
