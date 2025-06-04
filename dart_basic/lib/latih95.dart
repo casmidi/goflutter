93b62f02d6c8cf952d415d3a9898f712bbb04577
casmidi <casmidiasli@yahoo.co.id>
2025-06-04 15:10:34 +0700

    Penambahan file latih95.dart

---
+++ dart_basic/lib/latih95.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Integer List
+  List<int> ages = [10, 30, 23];
+
+  print(ages);
+
+  //String List
+  List<String> names = ["Raj", "John", "Rocky"];
+
+  print(names);
+
+  //Mixed List
+  var mixed = [10, "John", 18.8];
+
+  print(mixed);
+}
