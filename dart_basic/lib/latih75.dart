c0fa003df24f0c0341ffb1fce23aa93d74fb9d5c
casmidi <casmidiasli@yahoo.co.id>
2025-06-04 10:18:23 +0700

    Penambahan latih75.dart

---
+++ dart_basic/lib/latih75.dart
@@ -0,0 +1,12 @@
+int add(int a, int b) {
+  int sum = a + b;
+  return sum;
+}
+
+void main() {
+  int num1 = 10;
+  int num2 = 20;
+
+  int total = add(num1, num2);
+  print("The sum is $total.");
+}
