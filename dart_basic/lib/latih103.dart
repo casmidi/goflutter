637581d5e68fc44b9d51133646a575dad2389696
casmidi <casmidiasli@yahoo.co.id>
2025-06-05 11:08:29 +0700

    Penambahan latih103.dart

---
+++ dart_basic/lib/latih103.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> drinks = ["water", "jucie", "milk", "coke"];
+  List<int> ages = [];
+  print("is drinks Empty : " + drinks.isEmpty.toString());
+  print("is drinks not Empty:" + drinks.isNotEmpty.toString());
+  print("Is ages Empty: " + ages.isEmpty.toString());
+  print("Is ages not Empty : " + ages.isNotEmpty.toString());
+}
