ce38dcb2233b520a555b29b84402e90d74edb709
casmidi <casmidiasli@yahoo.co.id>
2025-06-09 13:38:46 +0700

    Example of Set Properties Dart

---
+++ dart_basic/lib/latih118.dart
@@ -0,0 +1,11 @@
+void main(){
+  //declaring fruits as Set
+  Set<String> fruits = {"Apple","Orange","Mangeo","Banana"};
+
+  //using different properties of set
+  print("First Value is ${fruits.first}");
+  print("Last Values is ${fruits.last}");
+  print("Is fruits empty ? ${fruits.isEmpty}");
+  print("Is fruits not mepty ? ${fruits.isNotEmpty}");
+  print("The length of fruits is ${fruits.length}");
+}
