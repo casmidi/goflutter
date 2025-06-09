83ee10d3fb36d8364c8f61e7f1051ae126c6409e
casmidi <casmidiasli@yahoo.co.id>
2025-06-09 13:50:42 +0700

    Add & Remove Items In Set

--- dart_basic/lib/latih120.dart
+++ dart_basic/lib/latih120.dart
@@ -3,10 +3,11 @@ void main() {
 
   fruits.add("Lemon");
   fruits.add("Grape");
+  fruits.add("Guava");
 
-  print("After Adding Lemon and grape: $fruits");
+  print("After Adding Lemon and grape And Guava: $fruits");
 
   fruits.remove("Apple");
 
-  print("After Removing Apple : $fruits");
+  print("After Removing Apple : $fruits"); //print After Removing Apple
 }
