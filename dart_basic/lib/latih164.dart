aca9aaeb94f78b77b07b3194c3c5ad401631b0e0
casmidi <casmidiasli@yahoo.co.id>
2025-06-10 08:53:58 +0700

    Default Constructor In Dart

---
+++ dart_basic/lib/latih164.dart
@@ -0,0 +1,15 @@
+class Laptop {
+  String? brand;
+  int? price;
+
+  //Contructor
+  Laptop() {
+    print("This is a default contructor");
+  }
+}
+
+void main() {
+  //Here laptop is object of class laptop
+  Laptop laptop = Laptop();
+ // print(laptop);
+}
