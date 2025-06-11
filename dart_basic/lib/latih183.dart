2898addccb87a2d5d243085b073c2314148fb69a
casmidi <casmidiasli@yahoo.co.id>
2025-06-11 16:21:41 +0700

    Setter In Dart With Data Validation

---
+++ dart_basic/lib/latih183.dart
@@ -0,0 +1,27 @@
+class NoteBook {
+  //Private Properties
+  String? _name;
+  double? _prize;
+
+  //Setter to update private property _name
+  set name(String name) => this._name = name;
+
+  //Setter to update private property _prize
+  set price(double price) => this._prize = price;
+
+  //Method to display the values of the properties
+  void display() {
+    print("Name: ${_name}");
+    print("Price: ${_prize}");
+  }
+}
+
+void main() {
+  //Create an object of Notebook class
+  NoteBook nb = NoteBook();
+  //Setting values to the object using setter
+  nb.name = "Dell";
+  nb.price = 500.00;
+  //Display the values of the object
+  nb.display();
+}
